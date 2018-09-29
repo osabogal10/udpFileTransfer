ebaa1be1ad657bcad1ea56463e38c74ad2e1e752
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-09-28 19:41:52 -0500

    udp

---
+++ ClientUDP.py
@@ -0,0 +1,116 @@
+import socket
+import sys
+import hashlib
+
+def md5Sum(filename, blocksize=1024):
+    hash = hashlib.md5()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(blocksize), b''):
+            hash.update(block)
+    return hash.hexdigest()
+
+Multicast_group = ('224.0.0.1',9001)
+Server_IP = (input('IP del servidor:'),9000)
+buffer_size = 1024
+
+
+def read_in_chunks(file_object, chunk_size=1024):
+    while True:
+        chunk_data = file_object.read(chunk_size)
+        if not chunk_data:
+            break
+        yield chunk_data
+
+def getSize(file_object):
+    file_object.seek(0,2)
+    size = file_object.tell()
+    file_object.seek(0)
+    return size
+
+
+
+try:
+    unicast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print('')
+    print('Socket unicast creado')
+except socket.error:
+    print('Fallo al crear socket unicast')
+    sys.exit()
+
+try:
+    multicast = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print('Socket Multicast creado')
+except socket.error:
+    print('Error al crear socket multicast')
+    sys.exit()
+
+try:
+    multicast.bind(Multicast_group)
+    print('Bind de Multicast completo')
+except socket.error:
+    print('Fallo bind de Multicast')
+    sys.exit()
+
+print('Esperando resuesta del servidor')
+
+Message = 'Esperando para recibir datos...'
+
+try:
+    unicast.sendto(Message, Server_IP)
+    d = unicast.recvfrom(buffer_size)
+    reply = d[0]
+    addr = d[1]
+
+    print('')
+    print('Respuesta del servidor: ',reply)
+except socket.error:
+    print('El servidor no responde')
+    sys.exit()
+
+size, Multicast_group = multicast.recvfrom(buffer_size)
+size = int(size)
+
+print('')
+print('Tamaño del archivo: ', str(size))
+print('')
+
+file = open('./dataClient/homeroRecibido.jpg', 'w')
+file_path = './dataClient/homeroRecibido.jpg'
+
+total = 0
+while True:
+    pieza, Multicast_group_loop = multicast.recvfrom(buffer_size)
+    file.write(pieza)
+
+    total += len(pieza)
+    print('Tamaño actual recibido: ',str(total))
+    if total == size:
+        break
+
+file.close()
+hashsum = md5Sum(file_path)
+
+print('')
+print('Hash : ', hashsum)
+
+print('')
+print('Transferencia terminada.')
+
+
+try:
+    unicast.close()
+    print('')
+    print('Socket unicast cerrado OK')
+except socket.error:
+    print('Fallo al cerrar el socket')
+    sys.exit()
+
+try:
+    multicast.close()
+    print('')
+    print('Socket multicast cerrado OK')
+except socket.error:
+    print('Fallo al cerrar el socket')
+    sys.exit()
+
+print('THE END Cliente :v')
---
+++ ServerUDP.py
@@ -0,0 +1,102 @@
+import socket
+import sys
+import time
+import hashlib
+
+def read_in_chunks(file_object, chunk_size=1024):
+    while True:
+        chunk_data = file_object.read(chunk_size)
+        if not chunk_data:
+            break
+        yield chunk_data
+
+def getSize(file_object):
+    file_object.seek(0,2)
+    size = file_object.tell()
+    file_object.seek(0)
+    return size
+
+def md5Sum(filename, blocksize=1024):
+    hash = hashlib.md5()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(blocksize), b''):
+            hash.update(block)
+    return hash.hexdigest()
+
+Multicast_group = ('224.0.0.1',9001)
+Server_IP = (input('IP del servidor:'),9000)
+Clients = 0
+Clients_to_join = int(input('Numero de clientes:'))
+buffer_size = 1024
+file_path = './dataServer/homero.jpg'
+addr = []
+
+try:
+    unicast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print('')
+    print('Socket unicast creado')
+except socket.error:
+    print('Fallo al crear socket unicast')
+    sys.exit()
+
+try:
+    unicast.bind(Server_IP)
+    print('Bind de unicast completo')
+except socket.error:
+    print('Fallo bind de unicast')
+    sys.exit()
+
+try:
+    multicast = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print('Socket Multicast creado')
+except socket.error:
+    print('Error al crear socket multicast')
+    sys.exit()
+
+print('Esperando resuesta del cliente')
+
+while Clients < Clients_to_join:
+    d = unicast.recvfrom(buffer_size)
+    data = d[0]
+    addr = d[1]
+
+    reply = 'Espere la transferencia (?'
+
+    unicast.sendto(reply, addr)
+
+    print('')
+    print('Respuesta del cliente desde: [', addr[0],': ',str(addr[1]),'] = ',data.strip())
+
+    Clients = Clients + 1
+
+if Clients == Clients_to_join:
+    print('')
+    print('- Enviando datos al siguiente grupo: ', str(Multicast_group))
+    print('- Enviando el siguiente archivo: ', str(file_path))
+
+    bytes = open(file_path)
+    size = getSize(bytes)
+
+    multicast.sendto(str(size), Multicast_group)
+
+    for pieza in read_in_chunks(bytes, 1024):
+        multicast.sendto(pieza, Multicast_group)
+        time.sleep(0.02)
+
+    hashsum = md5Sum(file_path)
+
+    print('')
+    print('Hash : ', hashsum)
+
+    print('')
+    print('Enviando datos ... ESPERE! ')
+
+try:
+    unicast.close()
+    print('')
+    print('Socket unicast cerrado OK')
+except socket.error:
+    print('Fallo al cerrar el socket')
+    sys.exit()
+
+print('THE END :v')
