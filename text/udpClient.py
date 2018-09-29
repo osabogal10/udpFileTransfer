0d706ac71998a7aa633718ce032b32e83cb9db20
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-09-28 20:22:38 -0500

    ip

--- text/udpClient.py
+++ text/udpClient.py
@@ -1,5 +1,5 @@
 from socket import *
-serverName = '127.0.0.1'
+serverName = '192.168.0.11'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
