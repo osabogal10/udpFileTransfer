95a5ce372b64f7c033414c2404a43f79905a7443
Juan Camilo Usche <jc.useche10@uniandes.edu.co>
2018-10-03 21:00:44 -0500

    rise

--- UDPClient.py
+++ UDPClient.py
@@ -1,8 +1,7 @@
-import socket, os, sys, threading, hashlib
+import socket, os, sys, threading, hashlib, time
+from time import gmtime, strftime
 
-from time import sleep
-
-SIZE=32768
+SIZE=60000
 hasher = hashlib.md5()
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,11 +13,12 @@ try:
     # Send data
     print('sending {!r}'.format(message))
     sent = sock.sendto(message, server_address)
-
+    start_time = time.time()
     #data = sock.recv(1024)
-    filename = './recibido/newfile1.jpg'
+    filename = './recibido/newfile1.mp3'
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     i = 0
+    bytesReceived = 0
     with open(filename, 'wb+') as f:
         data, address = sock.recvfrom(SIZE)
         while data != bytes(''.encode()):
@@ -29,7 +29,7 @@ try:
             print(i,data)
             i = i+1
             print('received {} bytes from {}'.format(len(data), address))
-
+            bytesReceived = bytesReceived + len(data)
             if data == b'Fin' :
                 print('Fin de la wea')
                 break
@@ -38,8 +38,31 @@ try:
         print('sali de loop')
         buf = f.read()
         hasher.update(buf)
+        hash_cliente = hasher.hexdigest()
         print('hash: ', hasher.hexdigest())
+        rcv_file_name, address = sock.recvfrom(SIZE)
+        print(rcv_file_name.decode('utf-8'))
+        miIdCliente, address = sock.recvfrom(SIZE)
+        print('CLIENTE: ',miIdCliente.decode('utf-8'))
+        hash_servidor, address = sock.recvfrom(SIZE)
+        hash_servidor = hash_servidor.decode('utf-8')
+        if hash_servidor == hash_cliente:
+            print('FILE DELIVERY: SUCCESS')
+        else:
+            print('FILE DELIVER: FAILURE')
+        bytesSent, address = sock.recvfrom(SIZE)
+        bytesSent = bytesSent.decode('utf-8')
+        print('SENT BYTES: ',bytesSent)
+        print('RECEIVED BYTES: ', bytesReceived)
+        numPaquetesServ, address = sock.recvfrom(SIZE)
+        numPaquetesServ = numPaquetesServ.decode('utf-8')
+        numPaquetesCli = i
+        print('PACKETS SEND: ', numPaquetesServ)
+        print('PACKETS RECEIVED: ', i)
+
+
     print('sali de open (?')
+    print(os.path.getsize(filename))
         #sent = sock.sendto(message, server_address)
         # Receive response
         #print('waiting to receive')
@@ -48,4 +71,9 @@ try:
 
 finally:
     print('closing socket')
+    elapsed_time = time.time() - start_time
+    showtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print(elapsed_time)
+    print(showtime)
+
     sock.close()
--- UDPServer.py
+++ UDPServer.py
@@ -1,7 +1,8 @@
-import socket, threading, os, hashlib
+import socket, threading, os, hashlib,sys
 from time import sleep
 
-SIZE=2048
+
+SIZE=60000
 hasher = hashlib.md5()
 # Create a UDP socket
 
@@ -11,24 +12,34 @@ class udp_transfer:
     # Bind the socket to the port
     def __init__(self):
         server_address = ('localhost', 10000)
+        num_clients = int(sys.argv[1])
+        file_name = sys.argv[2]
+
         print('starting up on {} port {}'.format(*server_address))
+
+        print('Argument List:', str(sys.argv))
         self.sock.bind(server_address)
         print('\nwaiting to receive message')
         threads = []
-        while True:
+        id_cliente =1
+        while num_clients > 0:
             data, address =self. sock.recvfrom(SIZE)
             if data == b'Listo':
-                file_name = 'imac.jpg'
                 size = os.path.getsize(file_name)
                 print(' file size : {}'.format(str(size)))
 
-                send_thread = threading.Thread(target=self.send_file, args=( address,))
-                send_thread.start()
+                send_thread = threading.Thread(target=self.send_file, args=( address, file_name, id_cliente))
+                threads.append(send_thread)
+                num_clients = num_clients-1
+                id_cliente =id_cliente+1
+                print(num_clients)
+        for thread in threads:
+            thread.start()
 
-    def send_file(self, address):
-        file_name = 'imac.jpg'
+    def send_file(self, address, file_name, id_cliente):
         i = 0
         size = os.path.getsize(file_name)
+        bytesSent = 0
         print(' file size : {}'.format(str(size)))
         with open(file_name, 'rb') as file:
             data = file.read(SIZE)
@@ -38,6 +49,7 @@ class udp_transfer:
                 print('data',data)
                 print('{}. sent {} bytes back to {}'.format(i,sent, address))
                 i = i +1
+                bytesSent = bytesSent+sent
                 if sent != SIZE:
                     sent = self.sock.sendto(b'Fin', address)
                     print('Fin')
@@ -45,7 +57,17 @@ class udp_transfer:
                     break
             buf = file.read()
             hasher.update(buf)
-            print(hasher.hexdigest())
+            hash_servidor = hasher.hexdigest()
+
+            self.sock.sendto(file_name.encode('utf-8'), address)
+            self.sock.sendto(str(id_cliente).encode('utf-8'), address)
+            self.sock.sendto(str(hash_servidor).encode('utf-8'), address)
+            self.sock.sendto(str(bytesSent).encode('utf-8'), address)
+            self.sock.sendto(str(i).encode('utf-8'), address)
+
+
+
+
 
 udp = udp_transfer()
 
