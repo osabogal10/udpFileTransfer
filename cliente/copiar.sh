1c8e2366b29ceb8893bebc90603cbbe6bb624f81
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-10-05 19:49:50 -0500

    ruta

--- cliente/copiar.sh
+++ cliente/copiar.sh
@@ -2,4 +2,4 @@ password="labredesML340"
 username="root"
 Ip="157.253.205.7"
 client="$(hostname)"
-sshpass -p $password scp -o StrictHostKeyChecking=no logs/UDP$client.log $username@$Ip:/home/s5g3/logs/
+sshpass -p $password scp -o StrictHostKeyChecking=no udpFileTransfer/cliente/logs/UDP$client.log $username@$Ip:/home/s5g3/logs/
